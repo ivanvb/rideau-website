cd8572c183106c910793f031f1d7505ba986a5f1
ivanvb <ivanvalette@hotmail.com>
2020-09-23 22:05:55 -0400

    initial gatsby setup

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,7 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
+    title: `Rideau`,
+    description: `Rideau, your reliable shades retailer. Offering innovative sun protection since 1980.`,
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
