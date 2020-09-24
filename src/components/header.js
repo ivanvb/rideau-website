a1269deed1d71c192a1789f8fc7d44b7ce812222
ivanvb <ivanvalette@hotmail.com>
2020-09-24 17:14:57 -0400

    Add animated responsive navbar
    * Add hamburger menu on small and medium screens.
    * Add animaiton to the hamburger menu when opening/closing it

--- src/components/header.js
+++ src/components/header.js
@@ -1,11 +1,13 @@
 import { Link } from "gatsby"
-import React from "react"
+import React, { useState, useRef } from "react"
 import RideauLogo from "../images/rideau-logo.svg"
 import { FaInstagram, FaTwitter, FaLinkedin } from "react-icons/fa"
+import { GiHamburgerMenu } from "react-icons/gi"
+import { MdClose } from "react-icons/md"
 
 const NavbarLink = ({ to, label }) => {
   return (
-    <li className="mx-3">
+    <li className="mx-0 lg:mx-3 py-2">
       <Link
         className="text-gray-500 text-lg hover:text-gray-600"
         activeClassName="text-blue-700 hover:text-blue-800"
@@ -19,7 +21,7 @@ const NavbarLink = ({ to, label }) => {
 
 const SocialMediaLink = ({ icon, url }) => {
   return (
-    <li className="mx-2">
+    <li className="mx-l-0 mr-4 lg:ml-2 py-2">
       <a href={url}>
         {React.cloneElement(icon, {
           size: 20,
@@ -68,21 +70,56 @@ const Header = () => {
       url: "https://www.linkedin.com",
     },
   ]
+
+  const [isOpen, setOpen] = useState(false)
+  const expandableSection = useRef(null)
+
+  const togggleNavbar = () => {
+    setOpen(prev => !prev)
+  }
+
   return (
-    <header className="bg-white shadow px-12 py-2 flex items-center justify-between">
-      <Link to="/" className="inline flex">
-        <img src={RideauLogo} alt="Rideau Logo" className="h-16" />
+    <header className="bg-white shadow px-3 lg:px-12 py-2 flex flex-wrap items-center justify-between sticky top-0">
+      <Link to="/" className="block flex-grow-0">
+        <img
+          src={RideauLogo}
+          alt="Rideau Logo"
+          className="h-16 transform transition-transform duration-200 active:scale-90"
+        />
       </Link>
-      <ul className="flex">
-        {navLinks.map((link, i) => (
-          <NavbarLink {...link} key={i} />
-        ))}
-      </ul>
-      <ul className="flex">
-        {socialMediaLinks.map((link, i) => (
-          <SocialMediaLink {...link} key={i} />
-        ))}
-      </ul>
+      <div className="absolute right-0 top-0 mr-3 mt-4 block lg:hidden">
+        <button onClick={togggleNavbar} className="p-1">
+          {isOpen ? (
+            <MdClose size={32} className="text-blue-600" />
+          ) : (
+            <GiHamburgerMenu size={32} className="text-blue-600" />
+          )}
+        </button>
+      </div>
+      <div
+        className={`
+          w-full lg:w-2/3 flex flex-col lg:flex-row lg:items-center justify-between overflow-y-hidden lg:overflow-y-visible transition-max-height duration-300 ${
+            isOpen ? "" : "max-h-0"
+          } lg:max-h-full
+        `}
+        style={{
+          maxHeight: isOpen
+            ? `${expandableSection.current.scrollHeight}px`
+            : "0",
+        }}
+        ref={expandableSection}
+      >
+        <ul className="flex w-full lg:w-auto flex-col lg:flex-row">
+          {navLinks.map((link, i) => (
+            <NavbarLink {...link} key={i} />
+          ))}
+        </ul>
+        <ul className="flex">
+          {socialMediaLinks.map((link, i) => (
+            <SocialMediaLink {...link} key={i} />
+          ))}
+        </ul>
+      </div>
     </header>
   )
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -7,6 +7,8 @@ module.exports = {
   theme: {
     extend: {},
   },
-  variants: {},
+  variants: {
+    scale: ["responsive", "hover", "focus", "active"],
+  },
   plugins: [],
 }
