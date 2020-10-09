0c1339cb6afd55bf2b2d8b6d84cd0a761d38734f
ivanvb <ivanvalette@hotmail.com>
2020-10-09 18:22:05 -0400

    Add read-more arrow

--- src/components/general/productTab.js
+++ src/components/general/productTab.js
@@ -1,5 +1,7 @@
 import React from "react"
 import Img from "gatsby-image"
+import { Link } from "gatsby"
+import { IoMdArrowRoundForward } from "react-icons/io"
 
 const ProductTab = ({ title, text, image }) => {
     return (
@@ -10,6 +12,15 @@ const ProductTab = ({ title, text, image }) => {
             <div className="w-full lg:w-1/2 my-3 lg:my-0">
                 <h2 className="section-title lg:-mt-2">{title}</h2>
                 <p className="text-gray-700 leading-7">{text}</p>
+                <div className="mt-4 flex items-center read-more-container">
+                    <Link
+                        to="/about"
+                        className="text-primary-500 font-bold flex items-center"
+                    >
+                        Read More
+                        <IoMdArrowRoundForward className="text-primary-500 mt-1 ml-1" />
+                    </Link>
+                </div>
             </div>
         </div>
     )
