d2c4a9ff2b93a50cb803d4055a4e38aa32fd55de
ivanvb <ivanvalette@hotmail.com>
2020-10-10 23:16:01 -0400

    add header image to categoryPage

--- src/templates/categoryPage.js
+++ src/templates/categoryPage.js
@@ -2,12 +2,15 @@ import React from "react"
 import Layout from "../components/layout"
 import { graphql } from "gatsby"
 import SEO from "../components/seo"
+import HeaderImage from "../components/general/headerImage"
 
-const CategoryPage = ({ data }) => {
+const CategoryPage = ({ data: { categoriesJson: categoryData } }) => {
     return (
         <Layout>
-            <SEO title={data.categoriesJson.name} />
-            {data.categoriesJson.name}
+            <SEO title={categoryData.name} />
+            <HeaderImage
+                fluidImage={categoryData.headerImage.childImageSharp.fluid}
+            />
         </Layout>
     )
 }
@@ -16,6 +19,13 @@ export const query = graphql`
     query($name: String!) {
         categoriesJson(name: { eq: $name }) {
             name
+            headerImage {
+                childImageSharp {
+                    fluid(maxWidth: 1440, maxHeight: 500) {
+                        ...GatsbyImageSharpFluid_tracedSVG
+                    }
+                }
+            }
         }
     }
 `
