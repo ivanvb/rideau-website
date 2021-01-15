6083fa4decb8672482f2035ef8d87e01dcedd7eb
ivanvb <ivanvalette@hotmail.com>
2021-01-15 19:45:49 -0400

    add frosted glass efect to categoryPage subcategories titles

--- src/templates/categoryPage.js
+++ src/templates/categoryPage.js
@@ -1,6 +1,6 @@
 import React from "react"
 import Layout from "../components/layout"
-import { graphql } from "gatsby"
+import { graphql, Link } from "gatsby"
 import SEO from "../components/seo"
 import HeaderImage from "../components/general/headerImage"
 import MultiparagraphText from "../components/general/multiparagraphText"
@@ -22,18 +22,35 @@ const CategoryPage = ({ data }) => {
                     {categoryData.subcategories.map((subcategory, i) => {
                         return (
                             <div className="col-span-6 md:col-span-4" key={i}>
-                                <div className="overflow-hidden shadow">
+                                <Link
+                                    to={`/products/${categoryData.name.toLowerCase()}/${subcategory.name.toLowerCase()}`}
+                                    className="overflow-hidden block relative"
+                                >
                                     <Img
                                         fluid={
                                             subcategory.picture.childImageSharp
                                                 .fluid
                                         }
-                                        className="hover:scale-110 transition-transform transform duration-150 cursor-pointer"
+                                        className="hover:scale-110 transition-transform transform duration-500 cursor-pointer"
                                     />
-                                </div>
-                                <span className="text-sm text-primary font-bold">
-                                    {subcategory.name}
-                                </span>
+                                    <div
+                                        className="absolute w-full h-8 lg:h-20 bottom-0 left-0 flex justify-start items-center pl-3 lg:pl-4"
+                                        style={{
+                                            backgroundColor: "rgba(0,0,0,0.35)",
+                                            backdropFilter: "blur(5px)",
+                                        }}
+                                    >
+                                        <h3
+                                            className="text-base lg:text-2xl text-white font-bold tracking-wider"
+                                            style={{
+                                                textShadow:
+                                                    "-1px 1px rgba(0, 0, 0, 0.25)",
+                                            }}
+                                        >
+                                            {subcategory.name}
+                                        </h3>
+                                    </div>
+                                </Link>
                             </div>
                         )
                     })}
