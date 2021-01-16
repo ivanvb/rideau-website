8dbef3cd2ac37692cb38807e46da065e48837bee
ivanvb <ivanvalette@hotmail.com>
2021-01-15 23:28:08 -0400

    add subcategories copy

---
+++ src/templates/subcategoryPage.js
@@ -0,0 +1,11 @@
+import React from "react"
+
+const subcategoryPage = () => {
+    return (
+        <div>
+            <p>Hello world</p>
+        </div>
+    )
+}
+
+export default subcategoryPage
