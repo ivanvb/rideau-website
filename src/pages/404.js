9a7c1fade1a3950bb95e256e0b885996049c6bd3
ivanvb <ivanvalette@hotmail.com>
2021-01-16 21:21:24 -0400

    update 404 page

--- src/pages/404.js
+++ src/pages/404.js
@@ -6,8 +6,14 @@ import SEO from "../components/seo"
 const NotFoundPage = () => (
     <Layout>
         <SEO title="404: Not found" />
-        <h1>404: Not Found</h1>
-        <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
+        <section className="std-padding-x">
+            <h1 className="text-primary-900 font-bold tracking-wider text-6xl text-center mt-16">
+                Oops!
+            </h1>
+            <p className="text-gray-700 mt-0 md:mt-4 text-center text-base md:text-xl mb-32">
+                We were not able to find what you are looking for
+            </p>
+        </section>
     </Layout>
 )
 
